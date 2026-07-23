3d98a103503ec9dcf17776810d2d1361d22d26e4
Gilad <rothgilad@gmail.com>
2026-07-24 00:26:25 +0300

    fix: add cleanupOutdatedCaches to purge old service worker caches

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
       workbox: {
         skipWaiting: true,
         clientsClaim: true,
+        cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
